d2f6fac7b541bbf3e3339636a501476c84e1598c
johnrscott <johnrscott0@gmail.com>
2018-05-30 15:44:46 +0100

    Added some member functions

--- data.h
+++ data.h
@@ -6,7 +6,7 @@ Filename: data.h
 Version: 1.0
 
 Created: 25th October 2016
-Last Modified: 25th October 2016
+Last Modified: 30th October 2018
 
 Language: C++ header
 
@@ -15,13 +15,25 @@ Language: C++ header
 #include <valarray>
 #include <complex>
 
-class timedomain
+// This class is designed to contain a digital signal. The dependent
+// variable is stored as an array and the independent variable is assumed
+// to have a fixed step size. The type can be used to store time domain
+// or frequency domain data.
+//
+class dsignal
 {
  private:
   valarray<complex> data;
-  valarray<double> time;
-
+  double indep_start;
+  double indep_end;
+  double indep_step;
+  
  public:
-  get_entry 
-
+  // Resize the signal array (does this make sense?) 
+  int get_size(void) { return data.size(); }
+  // Get the independent/dependent variable values at a point
+  int get_x(int N, double& value) { value = indep_start + N*indep_step; return 0; }
+  int get_y(int N, double& value) { value = data[N]; return 0; }
+    
+  }
 }
